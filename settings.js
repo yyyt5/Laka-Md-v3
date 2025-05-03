cd7d167c3aab8a37a1451378fc4291387cc5a2eb
mrlaka <lakarepo1@gmail.com>
2025-05-04 01:06:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-
 SESSION_ID: process.env.SESSION_ID || "session id",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
