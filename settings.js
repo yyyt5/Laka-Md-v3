b0e54b5bf5e9e36f2a0b4ef283e85702db4dc06d
yyyt5 <yrandula0@gmail.com>
2025-05-22 09:23:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=MxJz0Swa#Hasoh9qkmv3aHLzzeNT6MZNyQ4It33Oft136r5MkjFo",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=0gh22QrC#sHi61aGZUeT7i57KuBSgmImLjJ_LTFSnfcLjvRhP3i4",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
